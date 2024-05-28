8028b0cc43529d1cf2d22112dd51fc9b70c4f9a2
rahulsubh <102258593+rahulsubh@users.noreply.github.com>
2024-05-28 15:15:18 +0530

    Implemented circuit breaker and retry in resillience4j

---
+++ ApiGateway/ApiGateway/src/main/java/com/rahul/gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.rahul.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.net.http.HttpClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
--- UserService/UserService/src/main/java/com/rahul/user/service/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/rahul/user/service/controllers/UserController.java
@@ -2,7 +2,8 @@ package com.rahul.user.service.controllers;
 
 import com.rahul.user.service.entities.User;
 import com.rahul.user.service.services.UserService;
-import org.apache.coyote.Response;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,10 +24,28 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
+    int retryCount = 1;
+
     @GetMapping("/{userId}")
+    //@CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        System.out.println("Retry Count: " + retryCount);
+        retryCount++;
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //creating fallback method for circuitbreaker when service is down.
+    public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy because some service is down.")
+                .userId("141234")
+                .build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @GetMapping
