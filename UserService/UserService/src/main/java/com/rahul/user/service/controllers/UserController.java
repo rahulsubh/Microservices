5dce2140fb215d2dc34ed7cbd904685892ac0d80
rahulsubh <102258593+rahulsubh@users.noreply.github.com>
2024-05-28 15:41:23 +0530

    added rate limiter using resillience4j

--- UserService/UserService/src/main/java/com/rahul/user/service/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/rahul/user/service/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.rahul.user.service.controllers;
 import com.rahul.user.service.entities.User;
 import com.rahul.user.service.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,7 +29,8 @@ public class UserController {
 
     @GetMapping("/{userId}")
     //@CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
-    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    //@Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         System.out.println("Retry Count: " + retryCount);
         retryCount++;
